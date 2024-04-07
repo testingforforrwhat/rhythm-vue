c62c76b5a02c286f366f857254df849030c3ef5e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-08 00:50:28 +0800

    update

--- src/components/musicCategories/musicCategoriesList.vue
+++ src/components/musicCategories/musicCategoriesList.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Music Categories</h1>
     <ul>
-      <li v-for="category in musicCategories" :key="category.id">
-        {{ category.name }}
+      <li v-for="category in musicCategories" :key="category.categoryId">
+        {{ category.categoryName }}
       </li>
     </ul>
   </div>
@@ -25,7 +25,14 @@ export default {
     fetchMusicCategories() {
       axios.get('http://127.0.0.1:8001/api/categories')
           .then(response => {
-            this.musicCategories = response.data.data.musicCategoriesList;
+            this.musicCategories = response.data.data.musicCategoriesList.map(category => {
+              return {
+                categoryId: category.categoryId,
+                categoryName: category.categoryName,
+                createdAt: new Date(category.createdAt),
+                updatedAt: new Date(category.updatedAt)
+              };
+            });
           })
           .catch(error => {
             console.error('Failed to fetch music categories', error);
