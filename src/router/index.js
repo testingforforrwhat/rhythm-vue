9b2d40e4dc13329c3fc1448efc5bc2fd68cde31e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-25 13:15:59 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -9,19 +9,37 @@ const routes= [
         component: musicCayegories
     },
     {
-        path: '/musicCayegories',
-        name: 'musicCayegories',
-        component: musicCayegories
+        path: '/home',
+        name: '首页',
+        component: HomeView,
+        children: [
+            {
+                path: '/musicCayegories',
+                name: 'musicCayegories',
+                component: musicCayegories
+            },
+            {
+                path: '/musicCayegories',
+                name: 'musicCayegories',
+                component: musicCayegories
+            },
+            {
+                path: '/musicCayegories',
+                name: 'musicCayegories',
+                component: musicCayegories
+            },
+            {
+                path: '/musicCayegories',
+                name: 'musicCayegories',
+                component: musicCayegories
+            }
+        ]
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    /*这个地方就定义了地址和页面之间的映射关系
-    * 即如果浏览器地址栏是 / 则展示 HomeView 这个页面
-    * 如果浏览器地址栏是 /about，则展示 AboutView 这个页面
-    * */
-    routes
+    routes  // 定义地址和页面的映射
 })
 
 
