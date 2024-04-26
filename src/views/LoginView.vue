d1594323064d7684c8a4bfebda86168bd42b9358
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-26 14:00:30 +0800

    Revert "update"
    This reverts commit 8def0125df30d52034b3e6301e78c499950d4320.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@
     <div v-if="!isRegistered">
       <h2>Register</h2>
       <input type="text" v-model="registerUsername" placeholder="Username">
-      <input type="password" v-model="registerPassword" placeholder="Password/ReceivedmessageCode">
+      <input type="password" v-model="registerPassword" placeholder="Password">
       <button @click="register">Register</button>
     </div>
 
@@ -25,8 +25,6 @@
 
 <script>
 
-import axios from "axios";
-
 export default {
   name: "LoginView",
   data() {
@@ -44,48 +42,13 @@ export default {
     register() {
       // Perform registration logic here
       // After successful registration, set isRegistered to true
-
-      axios.get(`http://127.0.0.1:8001/sms/validate/${this.registerUsername}`)
-          .then(() => {
-            console.log('sending sms...');
-          })
-          .catch(error => {
-            console.error('Error send sms', error);
-          });
-
-      axios.post('http://127.0.0.1:8001/api/users/regist', {
-        userLoginName: this.registerUsername,
-        userValidate: this.registerPassword
-      })
-          .then(response => {
-            console.log('Registration successful:', response.data);
-            // After successful registration, set isRegistered to true
-            this.isRegistered = true;
-          })
-          .catch(error => {
-            console.error('Registration failed:', error);
-            // Handle registration error
-            alert('Registration failed,')
-          });
+      this.isRegistered = true;
     },
     login() {
       // Perform login logic here
       // After successful login, set isLoggedIn to true and store logged in user
-      axios.post('http://127.0.0.1:8001/api/users/login', {
-        username: this.loginUsername,
-        password: this.loginPassword
-      })
-          .then(response => {
-            console.log('Login successful:', response.data);
-            // After successful login, set isLoggedIn to true and store logged in user
-            this.isLoggedIn = true;
-            this.loggedInUser = this.loginUsername;
-          })
-          .catch(error => {
-            console.error('Login failed:', error);
-            alert("手机号或密码错误，请重新填写")
-            // Handle login error
-          });
+      this.isLoggedIn = true;
+      this.loggedInUser = this.loginUsername;
     },
     logout() {
       // Perform logout logic here
