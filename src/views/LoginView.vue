cb5a24416baaae417ab63d6956c84e34083dd713
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-27 13:03:51 +0800

    update login/localstorage/token

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -91,10 +91,10 @@ export default {
             this.loggedInUser = this.loginUsername;
 
             alert("Login successful")
-            // 将登陆成功后 服务器签发的token令牌 存入到本地缓存 LocalStorage
-            // 如果不存储token,登录也无法访问相关页面 index.js if( localStorage.getItem("token") == null )
+            // 将登陆成功后 服务器签发的token令牌 存入到本地缓存 LocalStorage(f12-application-storage-localstorage/sessionstorage)
+            // 如果不存储token,因为router index.js的设置, 登录也无法访问相关页面 index.js if( localStorage.getItem("token") == null )
             // LocalStorage本次session会话存储,关闭浏览器=回话关闭,localstorage存储数据empty
-            // localStorage.setItem("token", response.data.data);
+            localStorage.setItem("token", response.data.data);
             // 路由重定向到home首页
             router.push( { name : "首页" } )
           })
