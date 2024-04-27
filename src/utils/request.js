ba33617f79c16586ad054d26d32f3ec3135ead6e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-04-27 12:09:37 +0800

    update

---
+++ src/utils/request.js
@@ -0,0 +1,14 @@
+
+
+/*在这个文件中我们对请求和响应进行封装*/
+
+
+import axios from "axios";
+
+/**
+ * qs 模块是一个用于序列化和反序列化 URL 查询字符串的库。
+ * 通常与 Axios 等 HTTP 请求库一起使用，用于处理 URL 查询参数的编码和解码
+ */
+import qs from 'qs';
+
+import router from "@/router/index.js";
