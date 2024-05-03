d7275feff8202dd6cede582274cf110462acb8c0
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-04 04:55:09 +0800

    update vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
+// 从 vuex 模块 导入 创建仓库的方法
 import { createStore } from 'vuex'
 
+// 创建 总仓库 并且导出
 export default createStore({
+    // 各组件实例的data 迁移到这儿
     state: {
     },
+    // 各组件实例的(同步)方法 迁移到这儿
     mutations: {
     },
+    // 各组件实例的(异步)方法 迁移到这儿
     actions: {
     },
     modules: {
