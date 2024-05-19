28c4518b8ce26a40d56b7db3829600026ed0cd42
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-19 14:40:30 +0800

    update

---
+++ src/store/modules/music.js
@@ -0,0 +1,18 @@
+
+
+/*  music 业务模块 的 子仓库  */
+export default {
+    // 是否开启独立命名空间
+    // false 逻辑分区 (默认值)
+    // true  物理分区
+    namespaced: true,
+    // 各组件实例的data 迁移到这儿
+    state: {
+    },
+    // 各组件实例的(同步)方法 迁移到这儿
+    mutations: {
+    },
+    // 各组件实例的(异步)方法 迁移到这儿
+    actions: {
+    }
+}
