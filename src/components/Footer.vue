39f3323854ee1ea0ba227564a4674b4f40669175
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-05-22 15:24:57 +0800

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,13 +5,26 @@ export default {
 </script>
 
 <template>
-
-  <div class="footer">
-    this is footer
-  </div>
-
+  <footer class="footer">
+    <div class="container">
+      <p>&copy; 2024 Rhythm. </p>
+    </div>
+  </footer>
 </template>
 
-<style scoped>
+<style>
+.footer {
+  background-color: #333;
+  color: #fff;
+  padding: 15px 0;
+  text-align: center;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+}
 
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 </style>
