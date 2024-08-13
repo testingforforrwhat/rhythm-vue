611aca5c2db910362853e62d216826333d138caf
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-13 15:24:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,10 @@
 import HeaderView from "@/components/Header.vue";
 import FooterView from "@/components/Footer.vue";
 
+
+/*使用export语句只能导出已经命名的变量、函数或类。而使用export default语句的默认导出则可以导出任意表达式，包括匿名函数。
+
+一个模块只能有一个默认导出，因此export default在一个模块中只能使用一次。*/
 export default {
   name: 'App',
   components: {
@@ -28,6 +32,7 @@ export default {
   }
 }
 </script>
+
 
 <style>
 
