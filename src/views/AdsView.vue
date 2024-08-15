a25a72d2a130703703f9565718e90409529bb45a
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-15 15:45:04 +0800

    update 注册组件标签

--- src/views/AdsView.vue
+++ src/views/AdsView.vue
@@ -1,12 +1,19 @@
-<script>
-export default {
-  name: "AdsView"
-}
-</script>
+
 
 <template>
   AdsView
 </template>
+
+
+<script>
+
+export default {
+  // 注册组件标签
+  name: "AdsView"
+}
+
+</script>
+
 
 <style scoped>
 
