6552112333fe152e438b0c27ec7938dfda802311
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-18 20:52:49 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from "@/store";
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
 // 导入所有图标并全局注册
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 
 // 导入所有图标并全局注册
 // https://element-plus.org/zh-CN/component/icon.html
@@ -15,4 +17,4 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 }
 
 // 视图 挂载 模型数据
-app.use(router).use(ElementPlus).mount('#app')
+app.use(store).use(router).use(ElementPlus).mount('#app')
