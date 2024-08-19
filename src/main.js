7e0c07f24750336cb6b30d377719b6f6087488ea
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-19 23:52:43 +0800

    update vuex // main.js 导入我们创建的 store 并使用

--- src/main.js
+++ src/main.js
@@ -20,4 +20,10 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 // 视图 挂载 模型数据
 // 在你的 Vue 实例中导入并使用 Vuex Store
 // 在组件中，你可以通过 this.$store 来访问 Vuex Store，并通过相应的方法来读取状态、提交突变或者发起动作。
+/**
+ * this.$store.state.count  - data()
+ * this.$store.commit('increment')  - methods:
+ * this.$store.dispatch('incrementAsync')  - methods:
+ * this.$store.getters  - computed:
+ */
 app.use(store).use(router).use(ElementPlus).mount('#app')
