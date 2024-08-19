d237abf7c93c77edc01f3ee973b6c05836ba2a7e
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-08-19 23:42:01 +0800

    update vuex

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,23 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
 
+
+/**
+ *
+ * Vue.use(Vuex) 是在 Vue 2 中使用 Vuex 这个状态管理库的一个关键步骤。
+ * 它的作用是将 Vuex 插件安装到 Vue 框架中，使得 Vue 组件能够使用 Vuex 提供的状态管理功能。
+ *
+ * 在组件中，你可以通过 this.$store 来访问 Vuex Store，并通过相应的方法来读取状态、提交突变或者发起动作。
+ *
+ *
+ * Vue.use(Vuex) 是为在 Vue 2 中使用 Vuex 插件而必要的。
+ * 它的作用是将 Vuex 安装为 Vue 的插件，使全局的状态管理在组件中可用。
+ * 而在 Vue 3 中，这个步骤已经被内部简化和优化。
+ *
+ * 在 Vue 3 中，Vue.use(Vuex) 不再是必需的操作，因为 Vuex 被直接导入和使用，不需要显式安装插件
+ *
+ *
+ */
 Vue.use(Vuex)
 
 
